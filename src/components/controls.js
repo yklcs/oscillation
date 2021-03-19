9e3a1d59499838505bf1842a06b2b0f7fea1ae55
RocketLL <luc4sl33@gmail.com>
2021-03-20 00:48:49 +0900

    Update old changes

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React, { useContext, memo } from "react"
 
-import { card, controls } from "../styles/layout.module.css"
+import { card, controls, range, number, label, control } from "./layout.module.scss"
+import { OscillatorContext } from "./index"
 
-const Controls = () => (
-  <div className={`${card} ${controls}`}>
-    Test
+const Control = ({ txt, val, setVal, max = 1, min = 0, step = 0.01 }) => (
+  <div className={control}>
+    <span className={label}>{txt}</span>
+    <input type="range" className={range} value={val} onChange={(event) => { setVal(event.target.value) }} min={min} max={max} step={step} />
+    <input type="number" className={number} value={val} onChange={(event) => { setVal(event.target.value) }} />
   </div>
 )
 
-export default Controls
+const Controls = () => {
+  const { vars: { gamma, omega0, f0, omega, y0, v0 }, setVars } = useContext(OscillatorContext)
+  return (
+    <div className={`${card} ${controls}`}>
+      <Control txt={["y", <sub>0</sub>]} min={-500} max={500} step={1} val={y0} setVal={(x) => setVars({ y0: x, v0: v0, gamma: gamma, omega0: omega0, f0: f0, omega: omega })} />
+      <Control txt={["v", <sub>0</sub>]} min={-100} max={100} step={1} val={v0} setVal={(x) => setVars({ gamma: gamma, omega0: omega0, f0: f0, omega: omega, y0: y0, v0: x })} />
+      <Control txt={"γ"} val={gamma} setVal={(x) => setVars({ gamma: x, omega0: omega0, f0: f0, omega: omega, y0: y0, v0: v0 })} />
+      <Control txt={["ω", <sub>0</sub>]} val={omega0} setVal={(x) => setVars({ gamma: gamma, omega0: x, f0: f0, omega: omega, y0: y0, v0: v0 })} />
+      <Control txt={["F", <sub>0</sub>]} max={10} val={f0} setVal={(x) => setVars({ gamma: gamma, omega0: omega0, f0: x, omega: omega, y0: y0, v0: v0 })} />
+      <Control txt={"ω"} val={omega} setVal={(x) => setVars({ gamma: gamma, omega0: omega0, f0: f0, omega: x, y0: y0, v0: v0 })} />
+    </div >
+  )
+}
+
+export default memo(Controls)
--- src/components/oscillator.js
+++ src/components/oscillator.js
@@ -1,6 +1,7 @@
-import React, { useRef, Component, useEffect } from "react"
+import React, { useRef, useEffect, memo, useContext } from "react"
 
-import { card, oscillator } from "../styles/layout.module.css"
+import { OscillatorContext } from "./index"
+import { card, oscillator } from "./layout.module.scss"
 
 const w = 1000
 const h = 1000
@@ -15,11 +16,9 @@ const rk4 = f => (u, v, t, dt) => {
   return u + 1 / 6 * dt * (k1 + 2 * k2 + 2 * k3 + k4)
 }
 
-const draw = (ctx, t, state, trail) => {
-  const gamma = 0.01
-  const omega0 = 0.7
-  const F0 = 10
-  const omega = 0.2
+const draw = (ctx, t, vars, state, trail) => {
+  let gamma, omega0, f0, omega
+  [gamma, omega0, f0, omega] = vars
 
   if (start === undefined) {
     start = t
@@ -28,39 +27,65 @@ const draw = (ctx, t, state, trail) => {
 
   ctx.clearRect(0, 0, w, h)
 
-  let y, v, a, t_
-  [t_, y, v, a] = state
+  ctx.strokeStyle = "gray"
+  ctx.lineWidth = "1"
+
+  ctx.beginPath()
+  ctx.moveTo(0, 500)
+  ctx.lineTo(1000, 500)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(800, 0)
+  ctx.lineTo(800, 1000)
+  ctx.stroke()
+
+  let y, v, t_
+  [t_, y, v] = state
   const x = w * 0.8
   const dt = 0.1
 
-  v = rk4((t_, v_, y_) => F0 * Math.sin(omega * t_) - 2 * gamma * v_ - omega0 ** 2 * (y_ - 500))(v, y, t - start, dt)
+  v = rk4((t_, v_, y_) => f0 * Math.sin(omega * t_) - 2 * gamma * v_ - omega0 ** 2 * (y_ - h / 2))(v, y, t - start, dt)
   y = rk4((t_, y_, v_) => v_)(y, v, t - start, dt)
 
-  ctx.fillStyle = "black"
-  ctx.fillRect(x, y, 12, 12)
-
-  ctx.fillStyle = "blue"
-  trail.forEach(p => {
-    p[0] -= 1
-    ctx.fillRect(...p, 5, 5)
-  })
-
-  if (trail.length > 1000) {
-    trail.shift()
-  }
+  ctx.fillRect(x - 6, y - 6, 12, 12)
 
   trail.push([x, y])
 
+  ctx.strokeStyle = "blue"
+  ctx.lineWidth = "3"
+
+  ctx.beginPath()
+
+  for (let i = 0; i < trail.length - 4; i += 3) {
+    trail[i][0] -= 1
+    ctx.moveTo(trail[i][0], trail[i][1])
+    ctx.lineTo(trail[i + 3][0], trail[i + 3][1])
+  }
+
+  ctx.stroke()
+
+  if (trail.length > 800) {
+    trail.shift()
+    trail.shift()
+    trail.shift()
+  }
+
   t += dt
-  window.requestAnimationFrame(_ => draw(ctx, t + dt, [t, y, v, a], trail))
+  window.requestAnimationFrame(_ => draw(ctx, t + dt, vars, [t, y, v], trail))
 }
 
 const Oscillator = (t) => {
   const el = useRef(null)
+  const { vars: { gamma, omega0, f0, omega, y0, v0 } } = useContext(OscillatorContext)
 
   useEffect(() => {
     const ctx = el.current.getContext("2d")
-    window.requestAnimationFrame(_ => draw(ctx, 0, [0, 800, 0, 0], []))
+    ctx.fillStyle = "black"
+
+    const y0num = parseFloat(y0)
+    const v0num = parseFloat(v0)
+    window.requestAnimationFrame(_ => draw(ctx, 0, [gamma, omega0, f0, omega], [0, y0num + 500, v0num], []))
   })
 
   return (
@@ -70,4 +95,4 @@ const Oscillator = (t) => {
   )
 }
 
-export default Oscillator
+export default memo(Oscillator)
